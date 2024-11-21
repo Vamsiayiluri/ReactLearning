7f47d1d8cd0853b886a9a989e66f34e01b928566
Vamsiayiluri <vamsi.ayiluri@cognine.com>
2024-11-21 13:00:55 +0530

    Life cycle methods in functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import ClassLifeCycleMethods from "./components/ClassLifeCycleMethods";
+import FunctionalLifeCycleHooks from "./components/FunctionalLifeCycleHooks";
 
 function App() {
+  const person = {
+    name: "vamsi",
+    message: "Hello",
+    seatNumbers: [1, 2, 3],
+  };
   return (
     <div className="App">
-      <ClassLifeCycleMethods></ClassLifeCycleMethods>
+ <FunctionalLifeCycleHooks></FunctionalLifeCycleHooks>
     </div>
   );
 }
